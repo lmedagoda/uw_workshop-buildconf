0e9a54d7de3f10375da784ce90b9250118b97a66
lmedagoda <Lashika.Medagoda@dfki.de>
2017-03-04 13:46:54 +0100

    use system ruby

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 set -e
 sudo apt-get install ruby rubygems wget
 wget rock-robotics.org/autoproj_bootstrap
-ruby2.0 autoproj_bootstrap git git@github.com:lmedagoda/uw_workshop-buildconf.git
+ruby autoproj_bootstrap git git@github.com:lmedagoda/uw_workshop-buildconf.git
 . ./env.sh
 autoproj update
 autoproj build
